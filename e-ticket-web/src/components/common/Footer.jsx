c7727fd6cfd5bd70e8158df8b01b864588d7448b
JADA Mohamed <104516659+JadaMohamed@users.noreply.github.com>
2023-03-08 02:36:52 +0100

    Footer

--- e-ticket-web/src/components/common/Footer.jsx
+++ e-ticket-web/src/components/common/Footer.jsx
@@ -1,7 +1,67 @@
 import React from "react";
+import logo from "../../img/log-dark.svg";
 
+import "../../css/Footer.css";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <div className="footer-container">
+        <div className="footer-top">
+          <div className="platforme-infos">
+            <div className="logo">
+              <img src={logo} alt="" />
+            </div>
+            <div className="discreption">
+              Lorem ipsum dolor sit amet consectetur amet consectetur.
+            </div>
+            <div className="social-media">
+              <div className="facebook sm">
+                <i class="fa-brands fa-facebook-f"></i>
+              </div>
+              <div className="instagram sm">
+                <i class="fa-brands fa-instagram"></i>
+              </div>
+              <div className="twitter sm">
+                <i class="fa-brands fa-twitter"></i>
+              </div>
+            </div>
+          </div>
+          <div className="platforme-navigation">
+            <div className="navigator">
+              <div className="title-navigator">Navigate</div>
+              <div className="links">
+                <span>Home</span>
+                <span>Cart</span>
+                <span>My tickets</span>
+              </div>
+            </div>
+            <div className="navigator">
+              <div className="title-navigator">Events categories</div>
+              <div className="links">
+                <span>Festival | Concert</span>
+                <span>Family</span>
+                <span>Theater | Cinema</span>
+                <span>Sport</span>
+                <span>Course | Lecture</span>
+              </div>
+            </div>
+            <div className="navigator">
+              <div className="title-navigator">Authenticate</div>
+              <div className="links">
+                <span>Login</span>
+                <span>Sign Up</span>
+                <span>Become Organizer</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="footer-bottom">
+          <div className="copy-right">© E-ticket.com. All rights reserved.</div>
+          <div className="much-organized-events">2 387 organized event ;)</div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
