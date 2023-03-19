af2940b22b03aaa6ebc7993bf82993d6eba2124c
HafidElmoudden <abdulhafid858@gmail.com>
2023-03-19 01:29:35 +0000

    Download ticket is working without Seat Category

--- e-ticket-web/src/components/ticket/ticket.jsx
+++ e-ticket-web/src/components/ticket/ticket.jsx
@@ -1,102 +1,75 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "../../css/ticket.css";
+import TicketComponent from "../../organizer/components/create post form/ticket_comp";
+import { printComponent, printDiv } from "../../Utils/divToImage";
 import CountdownDate from "../common/countdown";
+
+import "../../organizer/css/create post form/ticket_form.css";
 
 function Ticket({ ticket, onClick, selectedTicket }) {
   const isSelected = ticket.ticket_id === selectedTicket?.ticket_id;
-
+  const ticketRef = useRef(null);
+  const downloadTicket = () => {
+    printDiv(ticketRef.current)
+  }
   return (
-    <div className={`ticket-card-table ${isSelected ? "selected" : ""}`}>
-      <div className="ticket-card">
-        <div className="ticket-card-container">
-          <div className="ticket-infos">
-            <input
-              type="checkbox"
-              name="selected-product"
-              className="disable-on-mobile"
-              title="Select"
-            />
-            <div className="preview-image">
-              <img src={ticket.Event.brand_url} alt="" />
-            </div>
-            <div className="event-inf">
-              <div className="event-title">{ticket.Event.title}</div>
+    <>
+    
+      <div className={`ticket-card-table ${isSelected ? "selected" : ""}`}>
+        <div className="ticket-card">
+          <div className="ticket-card-container">
+            <div className="ticket-infos">
+              <input
+                type="checkbox"
+                name="selected-product"
+                className="disable-on-mobile"
+                title="Select"
+              />
+              <div className="preview-image">
+                <img src={ticket.Event.brand_url} alt="" />
+              </div>
+              <div className="event-inf">
+                <div className="event-title">{ticket.Event.title}</div>
 
-              <div className="local inf">
-                <span className="material-symbols-outlined">location_on</span>
-                {ticket.Event.location}
-              </div>
-              <div className="date inf">
-                <span className="material-symbols-outlined">hourglass_top</span>
-                <CountdownDate date={ticket.Event.start_time} />
-              </div>
-              <div className="event-category">{ticket.Event.event_type}</div>
-            </div>
-          </div>
-          <div className="ticket-consult">
-            <div className="btns">
-              <div className="download-ticket btn">
-                <span className="material-symbols-outlined">download</span>
-                Download
-              </div>
-              <div
-                className="preview-ticket btn"
-                onClick={() => onClick(ticket)}
-              >
-                <span className="material-symbols-outlined">
-                  qr_code_scanner
-                </span>
-                View
+                <div className="local inf">
+                  <span className="material-symbols-outlined">location_on</span>
+                  {ticket.Event.location}
+                </div>
+                <div className="date inf">
+                  <span className="material-symbols-outlined">hourglass_top</span>
+                  <CountdownDate date={ticket.Event.start_time} />
+                </div>
+                <div className="event-category">{ticket.Event.event_type}</div>
               </div>
             </div>
-            <input
-              type="checkbox"
-              name="selected-product"
-              className="select-ticket-mobile"
-            />
+            <div className="ticket-consult">
+              <div className="btns">
+                <div className="download-ticket btn" onClick={() => downloadTicket()}>
+                  <span className="material-symbols-outlined">download</span>
+                  Download
+                </div>
+                <div
+                  className="preview-ticket btn"
+                  onClick={() => onClick(ticket)}
+                  style={{marginTop: 0}}
+                >
+                  <span className="material-symbols-outlined">
+                    qr_code_scanner
+                  </span>
+                  View
+                </div>
+              </div>
+              <input
+                type="checkbox"
+                name="selected-product"
+                className="select-ticket-mobile"
+              />
+            </div>
           </div>
         </div>
+      <TicketComponent eventData={ticket.Event} ref={ticketRef} qrCode={ticket.qrcode} width={"800px"} visible={false}/>
       </div>
-      {/* <div className="ticket-card">
-                <div className="ticket-card-container">
-                    <div className="ticket-infos">
-                        <input type="checkbox" name="selected-product" className="disable-on-mobile" title="Select" />
-                        <div className="preview-image">
-                            <img src="" alt="" />
-                        </div>
-                        <div className="event-inf">
-                            <div className="event-title">
-                                This suppose to be a title
-                            </div>
-                            <div className="event-category">
-                                Theater | Cinema
-                            </div>
-                            <div className="local inf">
-                                <span className="material-symbols-outlined">location_on</span>
-                                Megarama Agadir
-                            </div>
-                            <div className="date inf">
-                                <span className="material-symbols-outlined">hourglass_top</span>
-                                2022-05-12T23:50:21
-                            </div>
-                        </div>
-                    </div>
-                    <div className="ticket-consult">
-                        <div className="btns">
-                            <div className="download-ticket btn">
-                                <span className="material-symbols-outlined">download</span>
-                                Download
-                            </div>
-                            <div className="preview-ticket btn">
-                                <span className="material-symbols-outlined">qr_code_scanner</span>
-                                View
-                            </div>
-                        </div>
-                        <input type="checkbox" name="selected-product" className="select-ticket-mobile" />
-                    </div>
-                </div>
-            </div> */}
-    </div>
+    </>
   );
 }
 
