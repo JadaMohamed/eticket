767cbbe91d5a91bcfa0800c366b56bb924f8c533
JadaMohamed <jadamohameed@gmail.com>
2023-03-25 22:44:49 +0000

    Manage users (Admin)

--- e-ticket-web/src/Interfaces/Admin/Pages/manageusers.jsx
+++ e-ticket-web/src/Interfaces/Admin/Pages/manageusers.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import AdminNavigationBar from "../Components/navbar";
 import SearchUser from "../Components/search-users";
 import SideBar from "../Components/side_bar";
+import UsersGrid from "../Components/users-grid";
 import "../CSS/admin-manage-users.css";
 
 const AdminUserManager = () => {
@@ -12,10 +13,13 @@ const AdminUserManager = () => {
       <div className="admin-page">
         <SideBar activeBtn="users" />
         <div className="admin-page-content">
-          <SearchUser
+          <div className="left-side">
+            <SearchUser
             userTypeFilter={userTypeToSearchIn}
             setUserTypeFilter={setUserTypeToSearchIn}
-          />
+            />
+            <UsersGrid/>
+          </div>
         </div>
       </div>
     </div>
