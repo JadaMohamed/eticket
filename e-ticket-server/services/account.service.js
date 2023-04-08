21bcc3997af9214dcaaf0299ddf6bddd20709420
ELOUAIZI AYOUB <ayoubelouaizi@gmail.com>
2023-04-08 08:22:18 +0100

    first commit

--- e-ticket-server/services/account.service.js
+++ e-ticket-server/services/account.service.js
@@ -2,7 +2,6 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 
-
 const createAccount = async (data) => {
     return await prisma.account.create({ data });
 };
