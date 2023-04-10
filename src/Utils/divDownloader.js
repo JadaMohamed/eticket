b77ce97c860ee23105509a49d8367bb392b7454f
HafidElmoudden <abdulhafid858@gmail.com>
2023-04-10 14:11:55 +0000

    Download ticket as PDF

--- src/Utils/divDownloader.js
+++ src/Utils/divDownloader.js
@@ -1,6 +1,6 @@
 import html2canvas from 'html2canvas';
 import ReactDOMServer from 'react-dom/server';
-
+import jsPDF from 'jspdf';
 function printComponent(component) {
     if (!component) {
         console.error("you need to pass a valid React component", component);
@@ -27,15 +27,25 @@ function printDiv(div) {
         console.error("you need to pass a valid div element", div);
         return;
     }
-
-    html2canvas(div, { useCORS: true })
-        .then((canvas) => {
-            const dataUrl = canvas.toDataURL();
-            downloadURI(dataUrl, "ticket.png");
-        })
-        .catch((error) => {
-            console.error("Failed to create canvas image", error);
-        })
+    // We used to use this to print the ticket as image, now we print it as PDF rather.
+    // html2canvas(div, { useCORS: true })
+    //     .then((canvas) => {
+    //         const dataUrl = canvas.toDataURL();
+    //         downloadURI(dataUrl, "ticket.png");
+    //     })
+    //     .catch((error) => {
+    //         console.error("Failed to create canvas image", error);
+    //     })
+    html2canvas(div, { useCORS: true }).then((canvas) => {
+        const imgData = canvas.toDataURL("image/png");
+        const pdf = new jsPDF("p", "mm", "a4");
+        const imgWidth = 210;
+        const imgHeight = (canvas.height * imgWidth) / canvas.width;
+    
+        pdf.addImage(imgData, "PNG", 0, 0, imgWidth, imgHeight);
+    
+        pdf.save("ticket.pdf");
+      });
 }
 
 function downloadURI(uri, name) {
